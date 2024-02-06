68ae5d199e1065df3a954a7f1838b0a27f19de11
meteoreinger <reikathr@gmail.com>
2024-02-07 01:33:56 +0700

    Added delete to ProductController.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -40,6 +40,12 @@ public class ProductController {
         return "redirect:../list";
     }
 
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable String id) {
+        service.delete(id);
+        return "redirect:../list";
+    }
+
     @GetMapping("/list")
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
