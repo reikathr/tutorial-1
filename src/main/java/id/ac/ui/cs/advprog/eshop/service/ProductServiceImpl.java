4694d6f7efbe98d239693e5c7af0122c960cc30b
meteoreinger <reikathr@gmail.com>
2024-02-07 00:19:14 +0700

    Added findByID to ProductService.java and ProductServiceImpl.java

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -22,6 +22,11 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
+    public Product findByID(String id) {
+        return productRepository.findByID(id);
+    }
+
+    @Override
     public List<Product> findAll() {
         Iterator<Product> productIterator = productRepository.findAll();
         List<Product> allProduct = new ArrayList<>();
