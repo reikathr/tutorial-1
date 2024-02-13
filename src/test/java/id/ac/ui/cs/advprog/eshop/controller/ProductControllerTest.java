e5cacf74eae628fd870772c44cd4e825ef8ff398
meteoreinger <reikathr@gmail.com>
2024-02-13 23:32:25 +0700

    Added ProductControllerTest.java

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,74 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductControllerTest {
+
+    @InjectMocks
+    private ProductController productController;
+
+    @Mock
+    private ProductService service;
+
+    @Mock
+    private Model model;
+
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+    }
+
+    @Test
+    public void testCreateProductPage() {
+        String viewName = productController.createProductPage(model);
+        assertEquals("CreateProduct", viewName);
+    }
+
+    @Test
+    public void testCreateProductPost() {
+        String redirectURL = productController.createProductPost(product, model);
+        assertEquals("redirect:list", redirectURL);
+    }
+
+    @Test
+    public void testProductListPage() {
+        String viewName = productController.productListPage(model);
+        assertEquals("ProductList", viewName);
+    }
+
+    @Test
+    public void testEditProductPage() {
+        String productId = "eb558e9f-1c39-460e-8860-71af6af63bd6";
+        String viewName = productController.editProductPage(productId, model);
+        assertEquals("EditProduct", viewName);
+    }
+
+    @Test
+    public void testEditProductPut() {
+        String redirectURL = productController.editProductPut(product);
+        assertEquals("redirect:list", redirectURL);
+    }
+
+    @Test
+    public void testDeleteProduct() {
+        String productId = "eb558e9f-1c39-460e-8860-71af6af63bd6";
+        String redirectURL = productController.deleteProduct(productId);
+        assertEquals("redirect:../list", redirectURL);
+    }
+}
