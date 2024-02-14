9a1c7b7f42d8a76cbc03dc940177058fdd397101
meteoreinger <reikathr@gmail.com>
2024-02-14 17:35:58 +0700

    Modified visibility modifiers for two unit test classes

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomepageControllerTest.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HomepageControllerTest {
+class HomepageControllerTest {
     @Test
     void testHomepage() {
         HomepageController homepageController = new HomepageController();
