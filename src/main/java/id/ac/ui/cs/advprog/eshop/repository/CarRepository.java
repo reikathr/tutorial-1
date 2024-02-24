8b4f07c031c8729701b557809b89614fe89dfa52
meteoreinger <reikathr@gmail.com>
2024-02-24 16:46:33 +0700

    Make for loop in CarRepository.java update method consistent with the for loop in findById method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -30,8 +30,7 @@ public class CarRepository {
         return null;
     }
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
+        for (Car car : carData) {
             if (car.getCarId().equals(id)) {
                 // Update the existing car with the new information
                 car.setCarName(updatedCar.getCarName());
